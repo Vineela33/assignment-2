a18854aed3e3c0ac4e26895b8a3bc0ebe50ee980
Vineela33 <arepallivineelarohini@gmail.com>
2026-02-02 16:33:49 +0530

    Add files via upload

---
+++ ass2Task2.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total_sum = 0
+
+# Iterate from 1 to 50
+for num in range(1, 51):
+    total_sum += num
+
+# Print the result
+print("The sum of numbers from 1 to 50 is:", total_sum)
